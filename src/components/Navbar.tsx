b6563ebd139d5bab60b22c4671f501eef86ea8bf
Natalia <natnikiforuk@gmail.com>
2023-07-21 14:01:35 +0200

    Navbar created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+	return (
+		<div className="navbar">
+			<img className="navbar_logo" src="/src/assets/mail.png" alt="logo" />
+            <div className="navbar_header">uploadFile</div>
+		</div>
+	);
+};
+
+export default Navbar;
